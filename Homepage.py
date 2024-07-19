09f482866ca2d3b16da664b7814f71e25154d1cc
GANESH SHARMA <103425714+GANESHSHARMA1@users.noreply.github.com>
2024-07-19 22:26:29 +0530

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="JustClick",
+    page_icon="👉"
+)
+
+st.title("Welcome to ATS Checker.")
+st.sidebar.success("Welcome to JustClick-Home Page")
